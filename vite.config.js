998c905e338babcdce093e12b51e2e9ab058cb76
louiesan <louloumasterssj@gmail.com>
2025-06-01 00:30:21 +0100

    lall

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,6 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  base: "./",
-  plugins: [
-    tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "sections/*",
-          dest: "sections",
-        },
-        {
-          src: "src/*",
-          dest: "src",
-        },
-      ],
-    }),
-  ],
+  plugins: [tailwindcss()],
 });
